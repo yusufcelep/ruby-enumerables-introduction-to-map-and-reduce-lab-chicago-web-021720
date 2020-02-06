c054c876f3714d393e96cee4a551c81ff0a27f3a
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-06 20:08:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,13 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  
+  array = []
+  i = 0
+  while i < source_array.length do
+    array << source_array[i] * 2
+    i += 1
+  end
+  array
 end
 
 def map_to_square(source_array)
