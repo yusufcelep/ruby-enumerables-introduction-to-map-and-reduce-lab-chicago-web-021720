badbf6f8301090bed957a32d4dc3ee31efe04751
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-06 20:18:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,5 +60,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  
+  i = 0
+  while i < source_array.length do
+    if source_array[i]
+      return true
+    end
+    i += 1
+  end
+  return false
 end
