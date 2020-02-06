ab5cc9012dfcbff4655ada943a0e9e52fd2c8d7f
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-06 19:54:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# My Code here....
+def map_to_negativize(source_array)
+  
+end
+
+def map_to_change(source_array)
+  
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
+
