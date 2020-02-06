b61dfb2f055624ff791d99c7fa01adc39ecc2295
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-06 20:08:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,7 +29,13 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-  
+  array = []
+  i = 0
+  while i < source_array.length do
+    array << source_array[i] * 2
+    i += 1
+  end
+  array
 end
 
 def reduce_to_total(source_array, starting_point)
